a12c1a4bd9d458cb954fc238a0d1a9655caa2da5
Veronika Ten <verona.binko@gmail.com>
2023-08-12 00:16:19 +0300

    feat: add loading to CharList component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -42,39 +42,6 @@ class CharList extends Component {
               </li>
             );
           })}
-
-          {/* <li className="char__item char__item_selected">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li>
-          <li className="char__item">
-            <img src={thumbnail} alt="abyss" />
-            <div className="char__name">{name}</div>
-          </li> */}
         </ul>
         <button className="button button__main button__long">
           <div className="inner">load more</div>
@@ -83,53 +50,5 @@ class CharList extends Component {
     );
   }
 }
-
-// const CharList = () => {
-//     return (
-//         <div className="char__list">
-//             <ul className="char__grid">
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item char__item_selected">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//                 <li className="char__item">
-//                     <img src={abyss} alt="abyss"/>
-//                     <div className="char__name">Abyss</div>
-//                 </li>
-//             </ul>
-//             <button className="button button__main button__long">
-//                 <div className="inner">load more</div>
-//             </button>
-//         </div>
-//     )
-// }
 
 export default CharList;
