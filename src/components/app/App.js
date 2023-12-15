36b54fbcdbe6907126469fb12f1e2020a40576e9
Veronika Ten <verona.binko@gmail.com>
2023-12-15 11:40:58 +0300

    feat: rewrite CharList component from class to function

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -38,39 +38,3 @@ const App = () => {
 };
 
 export default App;
-
-// class App extends Component {
-//   state = {
-//     selectedChar: null,
-//   };
-
-//   onCharSelected = (id) => {
-//     this.setState({ selectedChar: id });
-//   };
-
-//   render() {
-//     return (
-//       <div className="app">
-//         <AppHeader />
-//         <main>
-//           <ErrorBoundary>
-//             <RandomChar />
-//           </ErrorBoundary>
-
-//           <div className="char__content">
-//             <ErrorBoundary>
-//               <CharList onCharSelected={this.onCharSelected} />
-//             </ErrorBoundary>
-
-//             <ErrorBoundary>
-//               <CharInfo charId={this.state.selectedChar} />
-//             </ErrorBoundary>
-//           </div>
-//           <img className="bg-decoration" src={decoration} alt="vision" />
-//         </main>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,105 +1,194 @@
 import "./charList.scss";
 // import abyss from "../../resources/img/abyss.jpg";
-import { Component, createRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 
-class CharList extends Component {
-  state = {
-    chars: [],
-    loading: true,
-    error: false,
-    newItemLoading: false,
-    offset: 210,
-    charEnded: false,
+const CharList = ({ onCharSelected }) => {
+  const [chars, setChars] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(210);
+  const [charEnded, setCharEnded] = useState(false);
+
+  const marvelService = new MarvelService();
+
+  useEffect(() => {
+    onRequest();
+  }, []);
+
+  const onRequest = (offset) => {
+    onCharListLoading();
+    marvelService.getAllCharacters(offset).then(showAllChars).catch(error);
   };
 
-  marvelService = new MarvelService();
-
-  componentDidMount = () => {
-    this.onRequest();
-    // this.addChars();
+  const onCharListLoading = () => {
+    setNewItemLoading(true);
   };
 
-  onRequest = (offset) => {
-    this.onCharListLoading();
-    this.marvelService
-      .getAllCharacters(offset)
-      .then(this.showAllChars)
-      .catch(this.onError);
+  const addChars = () => {
+    marvelService.getAllCharacters().then(showAllChars);
   };
 
-  onCharListLoading = () => {
-    this.setState({
-      newItemLoading: true,
-    });
-  };
-
-  addChars = () => {
-    this.marvelService.getAllCharacters().then(this.showAllChars);
-  };
-
-  showAllChars = (newCharList) => {
+  const showAllChars = (newCharList) => {
     let ended = false;
 
     if (newCharList.length < 9) {
       ended = true;
     }
-    this.setState(({ chars, offset, charEnded }) => ({
-      chars: [...chars, ...newCharList],
-      loading: false,
-      newItemLoading: false,
-      offset: offset + 9,
-      charEnded: ended,
-    }));
+
+    setChars([...chars, ...newCharList]);
+    setLoading(false);
+    setNewItemLoading(false);
+    setOffset((offset) => offset + 9);
+    setCharEnded(ended);
   };
 
-  setCharRef = (elem) => {
-    this.oneRef = elem;
+  // const setCharRef = (elem) => {
+  //   oneRef = elem;
+  //   console.log("elem", elem);
+  // };
 
-    // elem.current.classList.remove("char__item_selected");
-    console.log("elem", elem);
-  };
-
-  onCharHandleClick = (id) => {
+  const onCharHandleClick = (id) => {
     console.log("id", id);
-    this.props.onCharSelected(id);
+    onCharSelected(id);
   };
+  const loadingSpinner = loading ? <Spinner /> : null;
 
-  render() {
-    const { chars, loading, newItemLoading, offset, charEnded } = this.state;
-
-    const loadingSpinner = loading ? <Spinner /> : null;
-
-    return (
-      <div className="char__list">
-        <ul className="char__grid">
-          {loadingSpinner}
-          {chars.map((item) => {
-            return (
-              <li
-                key={item.id}
-                className="char__item"
-                ref={this.setCharRef}
-                onClick={() => this.onCharHandleClick(item.id)}
-              >
-                <img src={item.thumbnail} alt="abyss" />
-                <div className="char__name">{item.name}</div>
-              </li>
-            );
-          })}
-        </ul>
-        <button
-          className="button button__main button__long"
-          disabled={newItemLoading}
-          onClick={() => this.onRequest(offset)}
-          style={{ display: charEnded ? "none" : "block" }}
-        >
-          <div className="inner">load more</div>
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="char__list">
+      <ul className="char__grid">
+        {loadingSpinner}
+        {chars.map((item) => {
+          return (
+            <li
+              key={item.id}
+              className="char__item"
+              onClick={() => onCharHandleClick(item.id)}
+            >
+              <img src={item.thumbnail} alt="abyss" />
+              <div className="char__name">{item.name}</div>
+            </li>
+          );
+        })}
+      </ul>
+      <button
+        className="button button__main button__long"
+        disabled={newItemLoading}
+        onClick={() => onRequest(offset)}
+        style={{ display: charEnded ? "none" : "block" }}
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 export default CharList;
+
+// import "./charList.scss";
+// // import abyss from "../../resources/img/abyss.jpg";
+// import { Component, createRef } from "react";
+// import MarvelService from "../../services/MarvelService";
+// import Spinner from "../spinner/Spinner";
+
+// class CharList extends Component {
+//   state = {
+//     chars: [],
+//     loading: true,
+//     error: false,
+//     newItemLoading: false,
+//     offset: 210,
+//     charEnded: false,
+//   };
+
+//   marvelService = new MarvelService();
+
+//   componentDidMount = () => {
+//     this.onRequest();
+//     // this.addChars();
+//   };
+
+//   onRequest = (offset) => {
+//     this.onCharListLoading();
+//     this.marvelService
+//       .getAllCharacters(offset)
+//       .then(this.showAllChars)
+//       .catch(this.onError);
+//   };
+
+//   onCharListLoading = () => {
+//     this.setState({
+//       newItemLoading: true,
+//     });
+//   };
+
+//   addChars = () => {
+//     this.marvelService.getAllCharacters().then(this.showAllChars);
+//   };
+
+//   showAllChars = (newCharList) => {
+//     let ended = false;
+
+//     if (newCharList.length < 9) {
+//       ended = true;
+//     }
+//     this.setState(({ chars, offset }) => ({
+//       chars: [...chars, ...newCharList],
+//       loading: false,
+//       newItemLoading: false,
+//       offset: offset + 9,
+//       charEnded: ended,
+//     }));
+//   };
+
+//   setCharRef = (elem) => {
+//     this.oneRef = elem;
+
+//     // elem.current.classList.remove("char__item_selected");
+//     console.log("elem", elem);
+//   };
+
+//   onCharHandleClick = (id) => {
+//     console.log("id", id);
+//     this.props.onCharSelected(id);
+//   };
+
+//   render() {
+//     const { chars, loading, newItemLoading, offset, charEnded } = this.state;
+
+//     const loadingSpinner = loading ? <Spinner /> : null;
+
+//     return (
+//       <div className="char__list">
+//         <ul className="char__grid">
+//           {loadingSpinner}
+//           {chars.map((item) => {
+//             return (
+//               <li
+//                 key={item.id}
+//                 className="char__item"
+//                 ref={this.setCharRef}
+//                 onClick={() => this.onCharHandleClick(item.id)}
+//               >
+//                 <img src={item.thumbnail} alt="abyss" />
+//                 <div className="char__name">{item.name}</div>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//         <button
+//           className="button button__main button__long"
+//           disabled={newItemLoading}
+//           onClick={() => this.onRequest(offset)}
+//           style={{ display: charEnded ? "none" : "block" }}
+//         >
+//           <div className="inner">load more</div>
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default CharList;
